4d15e139f2039749fa676fa27db1bb0ee6d2f08f
Sven Kirschbaum (via Travis CI) <sven@kirschbaum.me>
2021-01-16 16:42:29 +0000

    Deploy fallobst22/react-stomp-hooks to github.com/fallobst22/react-stomp-hooks.git:gh-pages

---
+++ precache-manifest.ba69c4225ffc1fa08f53a0126a63b0ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fab778d2d3e27e491c62ebc4853045c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06358a5778adc0ad5c25",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "3be58a14d4c5a5db1432",
+    "url": "./static/js/2.670b4a60.chunk.js"
+  },
+  {
+    "revision": "2cbd273914339058f2633e51b55de39a",
+    "url": "./static/js/2.670b4a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06358a5778adc0ad5c25",
+    "url": "./static/js/main.4fee9f24.chunk.js"
+  },
+  {
+    "revision": "1832d3603c57f9e59a4e",
+    "url": "./static/js/runtime-main.5a89dee5.js"
+  }
+]);
